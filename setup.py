b6b1d42876abf6f031e17d9c0c83d0ce51dfb6f7
1ntegrale9 <1ntegrale9uation@gmail.com>
2020-01-14 13:58:05 +0900

    build: version up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="echidna",
-    version="0.1.1",
+    version="0.2.0",
     author="1ntegrale9",
     author_email="1ntegrale9uation@gmail.com",
     description="BOT and CLI framework for handling multiple services in python",
